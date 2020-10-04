9b3d50b589ef4cb6a416a9db4039c08b89101c89
Paavan Chopra <paavan012chopra@gmail.com>
2020-10-04 18:34:47 -0400

    add

--- terraform/vpc/provider.tf
+++ terraform/vpc/provider.tf
@@ -1,13 +1,13 @@
 
 provider "google" {
   credentials = "${var.GOOGLE_APPLICATION_CREDENTIALS_FILE}"
-  project     = "${var.gcp_project}"
+  project     = "${var.project}"
   region      = "${var.region}"
   
 } 
 
 provider "google-beta" {
   credentials = "${var.GOOGLE_APPLICATION_CREDENTIALS_FILE}"
-  project     = "${var.gcp_project}"
+  project     = "${var.project}"
   region      = "${var.region}"
 }

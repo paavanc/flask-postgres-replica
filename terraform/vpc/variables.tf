f5c70f4adf4757e5b747d1c60292689ca290956e
Paavan Chopra <paavan012chopra@gmail.com>
2020-10-04 18:17:34 -0400

    add

--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -33,7 +33,7 @@ variable "cidr_block" {
   description = "The IP address range of the VPC in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27."
   default     = "10.0.0.0/16"
   type        = string
-}
+} 
 
 variable "cidr_subnetwork_width_delta" {
   description = "The difference between your network and subnetwork netmask; an /16 network and a /20 subnetwork would be 4."

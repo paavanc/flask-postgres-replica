7fa935ae3caa53c6ca6d4df8e4ab19a913033567
Paavan Chopra <paavan012chopra@gmail.com>
2020-10-05 13:26:23 -0400

    update sa terraform

--- terraform/sa/main.tf
+++ terraform/sa/main.tf
@@ -4,17 +4,32 @@ resource "google_service_account" "service_account"{
   display_name= var.sa_name_display_name
 }
 
-data "google_iam_policy" "workload-identity"{
+data "google_iam_policy" "workload-identity-sa"{
 
   binding {
     role = "roles/iam.workloadIdentityUser"
     members = [
       "serviceAccount:${var.project}svc.id.goog[${var.name_space}/${var.k8_sa_account}]",
-      "serviceAccount:${google_service_account.service_account.email}"
     ]
   }
+}
+
+data "google_iam_policy" "workload-identity-project"{
+
   binding {
     role = "roles/secretmanager.secretAccessor"
-    members = ["serviceAccount:${google_service_account.service_account.email}"]
+    members = [
+      "serviceAccount:${google_service_account.service_account.email}]",
+    ]
   }
+}
+
+resource "google_service_account_iam_policy" "workload-identity-iam"{
+  service_account_id = google_service_account.service_account.name
+  policy_data = data.google_iam_policy.workload-identity-sa.policy_data
+}
+
+resource "google_project_iam_policy" "project-main"{
+  project = var.projects
+  policy_data  = data.google_iam_policy.workload-identity-project.policy_data
 }

996c6ac449b8d3b65461008b9b17b11cb7953309
Paavan Chopra <paavan012chopra@gmail.com>
2020-10-05 10:56:54 -0400

    raw sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,4 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-from flask_migrate import Migrate
-from flask_debugtoolbar import DebugToolbarExtension
-# from . import routes, models
-
-# Globally accessible libraries
-db = SQLAlchemy()
 
 
 def create_app():
@@ -14,13 +6,8 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-    # Initialize Plugins
-    db.init_app(app)
-
     with app.app_context():
         # Include our Routes
         from . import routes
-        toolbar = DebugToolbarExtension(app)
-        migrate = Migrate(app, db)
 
         return app
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,50 @@
 from datetime import datetime as dt
 
-from flask import current_app as app, request, make_response, jsonify, render_template
+from flask import current_app as app, request, jsonify
 from flask_basicauth import BasicAuth
-from . import db
-from .database.models.country import Country
-from .database.models.country_replica import CountryReplica
-from healthcheck import HealthCheck, EnvironmentDump
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
 
 basic_auth = BasicAuth(app)
 
-health = HealthCheck(app, "/health")
-envdump = EnvironmentDump(app, "/env")
+SELECT_FROM_QUERY = "SELECT * FROM country where two_letter = :x"
+CREATE_TEXT = "INSERT INTO COUNTRY(name, two_letter, country_id) VALUES (:name, :two_letter, :country_id)"
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+def get_sql_engine(config_key):
+    DB_URI = app.config[config_key]
+    engine = create_engine(DB_URI)
+    return engine
+
+def result_to_dict(resultproxy):
+    d, a = {}, []
+    for rowproxy in resultproxy:
+    # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in rowproxy.items():
+        # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+    return d
 
 
 @app.route('/status', methods=['GET'])
@@ -20,46 +54,44 @@ def hello_world():
     }
 
 
-@app.route('/country/', methods=['POST'])
+@app.route('/country', methods=['POST'])
 @basic_auth.required
 def create_user():
     """Create a country."""
-    data = request.get_json()
-    name = data['name']
-    country_id = data['country_id']
-    two_letter = data['two_letter']
     try:
-        new_country = Country(name=name,
-                              country_id=country_id,
-                              two_letter=two_letter)
-        db.session.add(new_country)  # Adds new User record to database
-        db.session.commit()  # Commits all changes
-        return {"success": data}
-
+        data = request.get_json()
+        name = data['name']
+        country_id = data['country_id']
+        two_letter = data['two_letter']
+        engine = get_sql_engine('SQLALCHEMY_DATABASE_URI')
+        with engine.connect() as conn:
+            conn.execute(text(CREATE_TEXT), name=name, two_letter=two_letter, country_id=country_id).fetchall()
+            return {"success": data}
     except BaseException as error:
-        #Not best practice!
         print('An exception occurred: {}'.format(error))
-        return {"error": "Failed to make Country"}
+        #Not best practice!
+        raise InvalidUsage("Failed to create Country")
 
 
 @app.route('/country/<two_letter>', methods=['GET'])
 @basic_auth.required
 def show_country(two_letter):
     try:
-        #country = Country.query.filter_by(two_letter=two_letter).first_or_404()
-        #return jsonify(country)
-        return Country.query().all()
+        engine = get_sql_engine('SQLALCHEMY_DATABASE_URI')
+        with engine.connect() as conn:
+            return result_to_dict(conn.execute(text(SELECT_FROM_QUERY), x=two_letter).fetchall())
     except BaseException as error:
         print('An exception occurred: {}'.format(error))
         #Not best practice!
-        return {"error": "Failed to get Country"}
+        raise InvalidUsage("Failed to get Country")
 @app.route('/country_replica/<two_letter>', methods=['GET'])
 @basic_auth.required
 def show_country_replica(two_letter):
     try:
-        country = CountryReplica.query.filter_by(two_letter=two_letter).first_or_404()
-        return jsonify(country)
+        engine = get_sql_engine('SQLALCHEMY_DATABASE_REPLICA_URI')
+        with engine.connect() as conn:
+            return result_to_dict(conn.execute(text(SELECT_FROM_QUERY), x=two_letter).fetchall())
     except BaseException as error:
         print('An exception occurred: {}'.format(error))
         #Not best practice!
-        return {"error": "Failed to get Country Replica"}
+        raise InvalidUsage("Failed to get Country Replica")
